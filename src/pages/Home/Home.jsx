7fe8917d90b28ecffeeb5e1f4d38e7493bbaf165
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-04-04 00:38:23 +0600

    Lottie animation file add

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Lottie from "lottie-react";
+import jobAnimation from "../../assets/lotteiFiles/Animation - 1743705276586.json"
 const Home = () => {
   return (
     <div>
       <h2>This is Home</h2>
+      <Lottie animationData={jobAnimation}></Lottie>
     </div>
   )
 }

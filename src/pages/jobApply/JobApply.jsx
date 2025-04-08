70365af895a5d5fd353c2e914b6c87e267c4ba3f
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-04-09 04:33:00 +0600

    applied jobs data send to backend

--- src/pages/jobApply/JobApply.jsx
+++ src/pages/jobApply/JobApply.jsx
@@ -1,19 +1,43 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { data, useParams } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
-
+import Swal from 'sweetalert2'
 const JobApply = () => {
     const{id}=useParams();
     const{user}= useAuth()
-    console.log(id,user)
+    //console.log(id,user)
     const handleJobSubmit=e=>{
         e.preventDefault();
         const form = e.target;
         const linkedIn = form.linkedin.value;
         const gitHub = form.github.value;
         const resume= form.resume.value;
-        console.log(linkedIn,gitHub,resume )
-        
+       // console.log(linkedIn,gitHub,resume )
+        const jobApplication = {
+            job_id:id,
+            applicant_email:user.email,
+            linkedIn,
+            gitHub,
+            resume
+        }
+        fetch('http://localhost:5000/job-applications',{
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(jobApplication)
+        })
+        .then(res=>res.json())
+        .then(data=>{
+            if(data.insertedId){
+                Swal.fire({
+                    title: "Good job!",
+                    text: "Apply job successfully",
+                    icon: "success"
+                  });
+            }
+            console.log(data)
+        })
     }
     return (
         <div className=" min-h-screen mt-2">
